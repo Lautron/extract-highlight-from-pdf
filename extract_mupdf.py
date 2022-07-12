2fb0c428dc13b2427dcd6208d0b839ea93f74664
Lautaro Bachmann <laurigbach@gmail.com>
2022-07-12 16:21:53 -0300

    Refactored some names and started using Highlight class

--- extract_mupdf.py
+++ extract_mupdf.py
@@ -1,11 +1,11 @@
-# Based on https://stackoverflow.com/a/62859169/562769
+# Based on https://stackoverflow.com/a/63686095
 
 from typing import List, Tuple
+from printer import HighlightFormatter, Highlight
 
 import fitz  # install with 'pip install pymupdf'
 
-
-def _parse_highlight(annot: fitz.Annot, wordlist: List[Tuple[float, float, float, float, str, int, int, int]]) -> str:
+def parse_text_from_annotation(annot: fitz.Annot, wordlist: List[Tuple[float, float, float, float, str, int, int, int]]) -> str:
     points = annot.vertices
     quad_count = int(len(points) / 4)
     sentences = []
@@ -18,31 +18,34 @@ def _parse_highlight(annot: fitz.Annot, wordlist: List[Tuple[float, float, float
     sentence = " ".join(sentences)
     return sentence
 
-
-def handle_page(page):
+def parse_highlight_from_page(page):
     wordlist = page.get_text("words")  # list of words on page
     wordlist.sort(key=lambda w: (w[3], w[0]))  # ascending y, then x
+    highlights: List[Highlight] = []
 
-    highlights = []
     annot = page.first_annot
     while annot:
-        if annot.type[0] == 8:
-            highlights.append(_parse_highlight(annot, wordlist))
-        print(annot.colors)
+        if annot.type[1] == 'Highlight':
+            annot_text = parse_text_from_annotation(annot, wordlist)
+            rgb_percentages = annot.colors['stroke']
+            #print(annot_text, annot.colors, sep='\n')
+            highlight = Highlight(rgb_percentages, annot_text)
+            highlights.append(highlight)
+        print(annot.type)
         annot = annot.next
     return highlights
 
-
-def main(filepath: str) -> List:
-    doc = fitz.open(filepath)
-
+def parse_highlight_from_pdf(filepath: str) -> List:
+    pdf = fitz.open(filepath)
     highlights = []
-    for page in doc:
-        highlights += handle_page(page)
+
+    for page in pdf:
+        highlights += parse_highlight_from_page(page)
 
     return highlights
 
 
 if __name__ == "__main__":
-    main("Clean Code - A Handbook of Agile Software Craftsmanship.pdf")
-    #print("\n\n".join(main("Clean Code - A Handbook of Agile Software Craftsmanship.pdf")))
+    #parse_highlight_from_pdf("Clean Code - A Handbook of Agile Software Craftsmanship.pdf")
+    print(parse_highlight_from_pdf("Clean Code - A Handbook of Agile Software Craftsmanship.pdf"))
+    #print('\n\n'.join(parse_highlight_from_pdf("Clean Code - A Handbook of Agile Software Craftsmanship.pdf")))
