8a6118e772d92639624480591afbe0668ba4f0f1
Lautaro Bachmann <laurigbach@gmail.com>
2022-07-14 17:47:59 -0300

    Added more test cases for Highlight and HighlightFormatter classes

--- test_printer.py
+++ test_printer.py
@@ -13,8 +13,11 @@ def _highlight(color, expected_hex):
 
 def test_Highlight():
     test_cases = [
+      ((1.0, 0.6666666865348816, 1.0), '#ffaaff'),
+      ((0.3333333432674408, 1.0, 1.0), '#55ffff'),
+      ((0.0, 1.0, 0.0), '#00ff00'),
+      ((0.3333333432674408, 0.6666666865348816, 1.0), '#55aaff'),
       ((1.0, 1.0, 0), '#ffff00'),
-      ((0.3333333432674408, 0.6666666865348816, 1.0), '#55aaff')
     ]
     for color, expected_hex in test_cases:
         _highlight(color, expected_hex)
@@ -27,8 +30,11 @@ def _printer(color, text, expected_result):
 def test_Printer():
     text = 'test'
     test_cases = [
+      ((1.0, 0.6666666865348816, 1.0),                f'\n# {text}'),
+      ((0.3333333432674408, 1.0, 1.0),                f'\n## {text}'),
+      ((0.0, 1.0, 0.0),                               f'\n### {text}'),
+      ((0.3333333432674408, 0.6666666865348816, 1.0), f'\n#### {text}'),
       ((1.0, 1.0, 0), f'{text}'),
-      ((0.3333333432674408, 0.6666666865348816, 1.0), f'# {text}')
     ]
     for color, expected_text in test_cases:
         _printer(color, text, expected_text)
